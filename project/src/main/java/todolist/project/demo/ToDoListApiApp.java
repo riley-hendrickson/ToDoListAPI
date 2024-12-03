8b9936ae951efb7c646e137d2bc8f20e2c7c600a
riley-hendrickson <rileyhe1@gmail.com>
2024-12-02 17:11:46 -0800

    updating progress

---
+++ project/src/main/java/todolist/project/demo/ToDoListApiApp.java
@@ -0,0 +1,13 @@
+package todolist.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApiApp 
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ToDoListApiApp.class, args);
+    }
+}
